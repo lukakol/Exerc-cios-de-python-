4e85679db62a839c87fed8c1e80f1ab5323c866b
lukakol <142332713+lukakol@users.noreply.github.com>
2024-10-02 23:11:31 -0300

    Add files via upload

--- Namorar_comigo.py
+++ Namorar_comigo.py
@@ -1,5 +1,10 @@
-n = str(input('Você quer namorar comigo: ')).strip().upper()
+# Solicita ao usuário que insira uma resposta à pergunta "Você quer namorar comigo?"
+n = input('Você quer namorar comigo? ').strip().upper()
+
+# Verifica se a resposta do usuário é "SIM"
 if n == 'SIM':
+    # Se a resposta for "SIM", imprime "Que bom!"
     print('Que bom!')
 else:
+    # Se a resposta não for "SIM", imprime "Nem queria mesmo"
     print('Nem queria mesmo')
